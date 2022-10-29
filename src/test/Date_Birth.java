083a2bd8a5f706572c3f99c67d3aa8c42af4a510
Fenixcor <fenixcorpo630@gmail.com>
2022-10-28 23:12:33 -0500

    Repositorio copiado cesar

---
+++ src/test/Date_Birth.java
@@ -0,0 +1,9 @@
+public class Date_Birth {
+
+    String Date_Birth = "04/04/1988";
+
+        @Test
+        public void happydayDate(){
+            System.out.printf("La fecha de nacimiento mia es: \n"+ Date_Birth +"\n");
+        }
+}
---
+++ src/test/java/Date_Birth.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class Date_Birth {
+    String Date_Birth = "04/04/1988";
+
+    @Test
+    public void DiaNacimiento(){
+        System.out.printf("La fecha de nacimiento mia es: \n"+ Date_Birth +"\n");
+    }
+}
